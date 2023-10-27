1bc5aabfc7f0347f19a18c4a3e63b0ded6539b58
Imanol Gomez <imanol@tuta.io>
2023-10-27 22:26:40 +0200

    Change model name

--- scripts/convert_model.py
+++ scripts/convert_model.py
@@ -1,8 +1,8 @@
 import tf2onnx
 
 # Convert the model
-model_proto, _ = tf2onnx.convert.from_keras(model_path="my_model2.h5")
+model_proto, _ = tf2onnx.convert.from_keras(model_path="../Models/traffic-sign-recognition-model.h5")
 
 # Save the ONNX model
-with open("model.onnx", "wb") as f:
+with open("traffic-sign-recognition-model.onnx", "wb") as f:
     f.write(model_proto.SerializeToString())
