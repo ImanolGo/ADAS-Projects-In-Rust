73dd32bc03d0bd914aa5c82d355e1fc702d4e0f0
Imanol Gomez <imanol@tuta.io>
2023-11-04 21:43:47 +0100

    Activity 2 works

--- Activity2-Traffic-Sign-Recognition/src/main.rs
+++ Activity2-Traffic-Sign-Recognition/src/main.rs
@@ -30,11 +30,6 @@ fn main() {
             // Load the image
             let img = image::open(img_path.clone()).unwrap();
 
-            // Crop to a square as in Python code
-            let (width, height) = img.dimensions();
-            let min_dimension = std::cmp::min(width, height);
-            let cropped_img = img.crop_imm(0, 0, min_dimension, min_dimension);
-
             // Resize the image to 30x30
             let resized_img = img.resize_exact(30, 30, image::imageops::FilterType::Nearest);
 
@@ -43,19 +38,16 @@ fn main() {
                 vec![rgb[0] as f32 / 255.0, rgb[1] as f32 / 255.0, rgb[2] as f32 / 255.0]
             }).collect();
 
-            // Confirming the size of the vector
-            println!("Size of vec after resizing: {}", vec.len());
 
             // Reshape the vector into an ndarray with shape (1, 30, 30, 3) to match Python's (batch, height, width, channels)
             let array: ndarray::Array4<f32> = ndarray::Array::from_shape_vec((1, 30, 30, 3), vec).unwrap();
-
-            // No need to permute axes, since we're now matching the Python order
 
             // Convert the array into a tensor
             let tensor = array.into_dyn().into_tensor();
 
             // Run the model - the tensor needs to be converted into a TValue
             let output = model.run(tvec!(tensor.into())).unwrap();
+
 
             // Process the output as in Python to find the predicted class ID
             let class_id = output[0]
@@ -66,8 +58,6 @@ fn main() {
                 .max_by(|a, b| a.1.partial_cmp(b.1).unwrap())
                 .map(|(index, _)| index)
                 .unwrap();
-            // Output the class id for verification
-            println!("Predicted class ID: {}", class_id);
 
             // Map the class id to its label
             let labels = [
