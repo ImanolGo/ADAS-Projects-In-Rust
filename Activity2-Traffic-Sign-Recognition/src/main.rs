e0b103228c007cbea9c8fd182e051b382fe5d250
Imanol Gomez <imanol@tuta.io>
2023-11-04 21:37:51 +0100

    First working

--- Activity2-Traffic-Sign-Recognition/src/main.rs
+++ Activity2-Traffic-Sign-Recognition/src/main.rs
@@ -4,47 +4,71 @@ extern crate image;
 use tract_onnx::prelude::*;
 use image::GenericImageView;
 use std::fs;
+use std::path::PathBuf;
 
-fn main() -> TractResult<()> {
+fn main() {
     // Load the ONNX model
     let model = tract_onnx::onnx()
-        .model_for_path("../data/Models/traffic-sign-recognition-model.onnx")?
-        .into_optimized()?
-        .into_runnable()?;
+        .model_for_path("../data/Models/traffic-sign-recognition-model.onnx").unwrap()
+        .into_optimized().unwrap()
+        .into_runnable().unwrap();
 
-    // Get list of files in the Tests directory
-    let paths = fs::read_dir("../data/Test").unwrap();
+     // Get list of file paths in the directory
+     let mut paths: Vec<PathBuf> = fs::read_dir("../data/Test")
+     .unwrap()
+     .map(|res| res.map(|e| e.path()))
+     .collect::<Result<Vec<_>, std::io::Error>>()
+     .unwrap();
 
-    for path in paths {
-        let img_path = path.unwrap().path();
-        if img_path.extension().unwrap() == "png" {
-            // Load and preprocess the image
-            let img = image::open(&img_path)?.resize(30, 30, image::imageops::FilterType::Nearest);
+    // Sort the paths
+    paths.sort();
 
-            let vec: Vec<f32> = img.pixels().flat_map(|(_, _, rgb)| {
-                vec![rgb[0] as f32, rgb[1] as f32, rgb[2] as f32]
+
+    // Iterate over the sorted paths
+    for img_path in paths {
+        if img_path.extension().map(|ext| ext == "png").unwrap_or(false) {
+            // Load the image
+            let img = image::open(img_path.clone()).unwrap();
+
+            // Crop to a square as in Python code
+            let (width, height) = img.dimensions();
+            let min_dimension = std::cmp::min(width, height);
+            let cropped_img = img.crop_imm(0, 0, min_dimension, min_dimension);
+
+            // Resize the image to 30x30
+            let resized_img = img.resize_exact(30, 30, image::imageops::FilterType::Nearest);
+
+            // Flatten and normalize pixel values
+            let vec: Vec<f32> = resized_img.pixels().flat_map(|(_, _, rgb)| {
+                vec![rgb[0] as f32 / 255.0, rgb[1] as f32 / 255.0, rgb[2] as f32 / 255.0]
             }).collect();
 
-            // Convert the Vec<f32> into an array (assuming the size is 1*3*30*30 = 2700)
-            let array: ndarray::Array<f32, _> = ndarray::Array::from_shape_vec((1, 3, 30, 30), vec)?;
+            // Confirming the size of the vector
+            println!("Size of vec after resizing: {}", vec.len());
 
-            // Convert the array into TValue
-            let tensor = array.into_tensor();
-            println!("Tensor shape: {:?}", tensor.shape());
+            // Reshape the vector into an ndarray with shape (1, 30, 30, 3) to match Python's (batch, height, width, channels)
+            let array: ndarray::Array4<f32> = ndarray::Array::from_shape_vec((1, 30, 30, 3), vec).unwrap();
 
-            // Run the model
-            let output = model.run(tvec![tensor.into()])?.remove(0);
+            // No need to permute axes, since we're now matching the Python order
 
-            // Get the maximum value's index = class id
-            let class_id = output
-                .to_array_view::<f32>()?
+            // Convert the array into a tensor
+            let tensor = array.into_dyn().into_tensor();
+
+            // Run the model - the tensor needs to be converted into a TValue
+            let output = model.run(tvec!(tensor.into())).unwrap();
+
+            // Process the output as in Python to find the predicted class ID
+            let class_id = output[0]
+                .to_array_view::<f32>()
+                .unwrap()
                 .iter()
                 .enumerate()
                 .max_by(|a, b| a.1.partial_cmp(b.1).unwrap())
                 .map(|(index, _)| index)
                 .unwrap();
+            // Output the class id for verification
+            println!("Predicted class ID: {}", class_id);
 
-            println!("Class ID: {}", class_id);
             // Map the class id to its label
             let labels = [
                 "Speed limit (20km/h)",
@@ -91,9 +115,17 @@ fn main() -> TractResult<()> {
                 "End of no passing",
                 "End no passing veh > 3.5 tons"
             ];
+
+            // Logging
+            //println!("Number of labels: {}", labels.len());
+            if class_id >= labels.len() {
+                println!("Error: Class ID is out of bounds for labels array. Class ID: {}", class_id);
+                continue;  // Skip this iteration to prevent panic
+            } else {
+                println!("Valid Class ID: {}", class_id);
+            }
+
             println!("File: {:?}, Predicted: {}", img_path, labels[class_id]);
         }
     }
-
-    Ok(())
 }
